a3b6195db980117798b22c7db622d9460a0eb265
anushkagaggar <anumah23@gmail.com>
2026-01-29 01:19:39 +0530

    JWT Middleware Protection implemented ie it checks only logged in users can use protected APIs

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.config import settings
 from app.routes import auth
+from app.middleware.auth import get_current_user
+from app.models.user import User
+from fastapi import Depends
 
 app = FastAPI()
 app.include_router(auth.router)
@@ -11,4 +14,11 @@ def health():
     return {
         "status": "ok",
         "env": settings.ENV
+    }
+
+@app.get("/me")
+async def me(current_user: User = Depends(get_current_user)):
+    return {
+        "id": current_user.id,
+        "email": current_user.email
     }
