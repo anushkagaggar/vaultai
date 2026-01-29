64494089b00fcbc169741b1733327b44109b70cf
anushkagaggar <anumah23@gmail.com>
2026-01-29 17:05:48 +0530

    update expense feature added to api

--- backend/app/routes/expenses.py
+++ backend/app/routes/expenses.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import get_db
@@ -8,7 +8,7 @@ from app.models.user import User
 from app.schemas.expense import ExpenseCreate, ExpenseOut
 from sqlalchemy.future import select
 from sqlalchemy import desc
-
+from app.schemas.expense import ExpenseUpdate
 router = APIRouter(prefix="/expenses", tags=["expenses"])
 
 
@@ -59,3 +59,33 @@ async def list_expenses(
     expenses = result.scalars().all()
 
     return expenses
+
+@router.put("/{expense_id}", response_model=ExpenseOut)
+async def update_expense(
+    expense_id: int,
+    expense: ExpenseUpdate,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+
+    result = await db.execute(
+        select(Expense).where(
+            Expense.id == expense_id,
+            Expense.user_id == current_user.id,
+        )
+    )
+
+    db_expense = result.scalar_one_or_none()
+
+    if not db_expense:
+        raise HTTPException(status_code=404, detail="Expense not found")
+
+    data = expense.model_dump(exclude_unset=True)
+
+    for key, value in data.items():
+        setattr(db_expense, key, value)
+
+    await db.commit()
+    await db.refresh(db_expense)
+
+    return db_expense
