162c8c69fe59bc8cf4b0b9d87edbb7366cd2dd18
anushkagaggar <anumah23@gmail.com>
2026-01-30 22:23:18 +0530

    we cant use supabase prod DB for testing so created a separate test db using SQLite

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,5 +1,43 @@
 import sys
 import os
+import pytest
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
+
+from app.main import app
+from app.database import get_db
+from app.models.base import Base
+
+
+TEST_DB_URL = "sqlite+aiosqlite:///./test.db"
+
+engine = create_async_engine(TEST_DB_URL, future=True)
+
+TestingSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+
+async def override_get_db():
+    async with TestingSessionLocal() as session:
+        yield session
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def setup_db():
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    await engine.dispose()
