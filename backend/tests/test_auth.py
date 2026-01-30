a3dcd0134f3b560f0b23ad035790a8ed7d0d8dbf
anushkagaggar <anumah23@gmail.com>
2026-01-30 22:18:23 +0530

    version mismatch error of app in pytest for testing purpose conflict resolution

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,12 +1,17 @@
 import pytest
-from httpx import AsyncClient
+from httpx import AsyncClient, ASGITransport
 from app.main import app
 
 
 @pytest.mark.asyncio
 async def test_register_and_login():
 
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    transport = ASGITransport(app=app)
+
+    async with AsyncClient(
+        transport=transport,
+        base_url="http://test"
+    ) as ac:
 
         # Register
         res = await ac.post("/auth/register", json={
--- backend/tests/test_expenses.py
+++ backend/tests/test_expenses.py
@@ -1,12 +1,17 @@
 import pytest
-from httpx import AsyncClient
+from httpx import AsyncClient, ASGITransport
 from app.main import app
 
 
 @pytest.mark.asyncio
 async def test_expense_crud():
 
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    transport = ASGITransport(app=app)
+
+    async with AsyncClient(
+        transport=transport,
+        base_url="http://test"
+    ) as ac:
 
         # Login
         login = await ac.post("/auth/login", json={
